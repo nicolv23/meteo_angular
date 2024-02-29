075c8b36c0e26846082b8525a863b1a27bc0d03d
Killua2000 <nicolaslv0018@gmail.com>
2024-02-28 19:53:01 -0500

    Modification du style CSS pour bien afficher la page principale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importez CommonModule
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  // Assurez-vous d'inclure RouterOutlet et CommonModule dans les imports de votre composant
+  imports: [RouterOutlet, CommonModule]
 })
 export class AppComponent {
   title = 'AppMeteo';
