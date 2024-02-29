74d0b0a48184318cf576eb503bfa8874171229d6
Killua2000 <nicolaslv0018@gmail.com>
2024-02-28 22:34:13 -0500

    Ajout du modeles de InformationMeteo recuperant les données json en TypeScript

--- src/app/services/meteo.service.ts
+++ src/app/services/meteo.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environnement } from '../environnements/environnement';
+import { InformationMeteo } from '../models/meteo.models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,8 @@ export class MeteoService {
 
   constructor(private http: HttpClient) { }
 
-  getInformationMeteo(nomVille: string){
-    this.http.get(environnement.weatherApiBaseUrl, {
+  getInformationMeteo(nomVille: string): Observable<InformationMeteo> {
+    return this.http.get<InformationMeteo>(environnement.weatherApiBaseUrl, {
       headers: new HttpHeaders()
       .set(environnement.XRapidAPIHostHeaderName, environnement.
       XRapidAPIHostHeaderValue)
